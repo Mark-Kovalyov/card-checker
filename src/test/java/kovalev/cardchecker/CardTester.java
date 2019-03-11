c0d47b1d4ddb0f591535966ea8c95769074e7455
mayton <mark.kovalyov78@gmail.com>
2019-03-12 01:01:26 +0200

    Add test data

---
+++ src/test/java/kovalev/cardchecker/CardTester.java
@@ -0,0 +1,15 @@
+package kovalev.cardchecker;
+
+import org.junit.Test;
+
+public class CardTester {
+
+    @Test
+    public void test() {
+        // 5168 7554 2103 4394
+        // 4149 4978 6871 3779
+        // 5168 7573 3310 7425
+        // 5168 7422 1127 0610
+    }
+
+}
