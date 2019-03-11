e2b58e30526b7ed8152da48e56eb37fe9e8d8784
mayton <mark.kovalyov78@gmail.com>
2019-03-12 01:20:39 +0200

    Add implementation. Tests is OK!

--- src/main/java/kovalev/cardchecker/BankCardService.java
+++ src/main/java/kovalev/cardchecker/BankCardService.java
@@ -22,7 +22,30 @@ public class BankCardService {
     public boolean check(String cardNumber) {
         if (cardNumber==null) throw new IllegalArgumentException("cardNumber cannot be null");
         if (cardNumber.length()!=19) throw new IllegalArgumentException("cardNumber must be equals to ####-####-####-####");
-        return true;
+        int pos = 0;
+        int sum = 0;
+        for (int i = 0; i < cardNumber.length(); i++) {
+            int c = cardNumber.charAt(i);
+            if (c >= '0' && c <= '9') {
+                if ((pos & 1) == 0) {
+                    int res = ((c - '0') << 1);
+                    if (res >= 10) {
+                        sum += res %10;
+                        sum++;
+                    } else {
+                        sum += res;
+                    }
+                } else {
+                    sum += (c - '0');
+                }
+                pos++;
+            } else {
+                if (i==4 || i==9 || i==14) {
+                    if (c!='-') throw new IllegalArgumentException("cardNumber must be equals to ####-####-####-####");
+                }
+            }
+        }
+        return sum % 10 == 0;
     }
 
 
