04ddc08ae82feadc3e335b86db75ed5c22a9af77
mayton <mark.kovalyov78@gmail.com>
2019-03-12 00:16:15 +0200

    After Spring Initializr

---
+++ src/main/java/kovalev/cardchecker/CardCheckerApplication.java
@@ -0,0 +1,13 @@
+package kovalev.cardchecker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardCheckerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardCheckerApplication.class, args);
+	}
+
+}
