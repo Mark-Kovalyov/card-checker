48e8f60a0e2b0b0009df9aa3442c2bd0c1490cb2
mayton <mark.kovalyov78@gmail.com>
2019-03-12 01:14:30 +0200

    Add Junit tests theory

--- src/test/java/kovalev/cardchecker/CardTester.java
+++ src/test/java/kovalev/cardchecker/CardTester.java
@@ -1,15 +1,27 @@
 package kovalev.cardchecker;
 
-import org.junit.Test;
+import org.junit.experimental.theories.DataPoints;
+import org.junit.experimental.theories.Theories;
+import org.junit.experimental.theories.Theory;
+import org.junit.runner.RunWith;
 
+import static org.junit.Assert.assertTrue;
+
+@RunWith(Theories.class)
 public class CardTester {
 
-    @Test
-    public void test() {
-        // 5168 7554 2103 4394
-        // 4149 4978 6871 3779
-        // 5168 7573 3310 7425
-        // 5168 7422 1127 0610
+    @DataPoints
+    public final static String[] numbers = { 
+            "5168-7554-2103-4394",
+            "4149-4978-6871-3779",
+            "5168-7573-3310-7425",
+            "5168-7422-1127-0610"
+    };
+
+    @Theory
+    public void bankCardIsCorrect(String cardNumber) {
+        BankCardService bankCardService = BankCardService.createInstance();
+        assertTrue(bankCardService.check(cardNumber));
     }
 
 }
