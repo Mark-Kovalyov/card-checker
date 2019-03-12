2c8184f8ba8fbcb1b30643f92ac4b390f3881ed4
mayton <mark.kovalyov78@gmail.com>
2019-03-12 02:28:50 +0200

    Add comments, urls.

--- src/main/java/kovalev/cardchecker/CardCheckerApplication.java
+++ src/main/java/kovalev/cardchecker/CardCheckerApplication.java
@@ -6,6 +6,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CardCheckerApplication {
 
+	/**
+	 * http://127.0.0.1:8084/hystrix
+	 *
+	 * http://127.0.0.1:8084/actuator
+	 *
+	 * http://127.0.0.1:8084/actuator/hystrix.stream
+	 *
+	 *
+	 * @param args
+	 */
+
 	public static void main(String[] args) {
 		SpringApplication.run(CardCheckerApplication.class, args);
 	}
