a2b64ef6ee5be77810a2db45329587509df5968d
mayton <mark.kovalyov78@gmail.com>
2019-03-12 02:34:48 +0200

    Add hystrix callback handlers.

--- src/main/java/kovalev/cardchecker/CardController.java
+++ src/main/java/kovalev/cardchecker/CardController.java
@@ -1,6 +1,7 @@
 package kovalev.cardchecker;
 
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,6 +15,9 @@ public class CardController {
     static Logger logger = LoggerFactory.getLogger(CardController.class);
 
     @RequestMapping(value = "/check/{card}", method = RequestMethod.GET, produces = "application/json")
+    @HystrixCommand(fallbackMethod = "fallbackCheck",
+                    commandKey = "cardKey",
+                    groupKey = "cardGroup")
     public CheckResult check(@PathVariable("card") String card) {
         logger.info(":: card = {}", card);
         boolean result = false;
@@ -23,6 +27,10 @@ public class CardController {
             return new CheckResult(false, ex.getMessage());
         }
         return new CheckResult(result);
+    }
+
+    public CheckResult fallbackCheck(String card) {
+        return new CheckResult(false, "fallback");
     }
 
 }
